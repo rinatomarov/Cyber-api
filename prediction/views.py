fbaa7c2d99d4fc114b7350e25a79cce8fee7937e
rinat <rinfisd@gmail.com>
2021-04-14 01:11:02 +0600

    Adding  ML model

--- prediction/views.py
+++ prediction/views.py
@@ -7,6 +7,7 @@ from nltk.tokenize import word_tokenize
 from posts.models import Post, Category
 import nltk
 import pickle
+import sklearn
 
 nltk.download('punkt')
 
@@ -22,17 +23,27 @@ from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 from users.models import User
 
-from sklearn.externals import joblib
+import joblib
 
+
+def load():
+    with open('prediction/model.pkl', 'rb') as file:
+        vectorizer, clf = pickle.load(file)
+    return vectorizer, clf
 
 class PredictionView(APIView):
+
     def post(self, request):
         serializer = DataSerializer(data=request.data)
         if serializer.is_valid():
             title = serializer.data.get('title')
-            model = pickle.load(open('prediction/model_final.pkl', 'rb'))
-            pm = process_message(title)
-            outcome = model.classify(pm)
+            vectorizer11, classifer11 = load()
+            #
+            vectorize_message = vectorizer11.transform([title])
+            outcome = classifer11.predict(vectorize_message)[0]
+            # predict_proba = classifer11.predict_proba(vectorize_message).tolist()
+            # pm = process_message(title)
+            # outcome = model.classify(pm)
             user = User.objects.get(id=1)
             cat = Category.objects.get(id=1)
             if outcome:
@@ -42,22 +53,4 @@ class PredictionView(APIView):
                 'result': outcome,
             }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-def process_message(message, lower_case=True, stem=True, stop_words=True, gram=2):
-    if not lower_case:
-        message = message.lower()
-    words = word_tokenize(message)
-    words = [w for w in words if len(w) > 2]
-    if gram > 1:
-        w = []
-        for i in range(len(words) - gram + 1):
-            w += [' '.join(words[i:i + gram])]
-        return w
-    if stop_words:
-        sw = stopwords.words("russian")
-    if stem:
-        stemmer = SnowballStemmer("russian")
-        words = [stemmer.stem(word) for word in words]
-    print(words)
-    return words
+print(sklearn.__version__)
